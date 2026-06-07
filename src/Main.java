b41774b1c8508028ad2d0d9d9608fd89c4526c24
Sebastian Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-06-07 15:31:50 -0500

    Actualizando Calculadora con entradas de usuario

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
-// Git y Github Colaborativo Actividad
-// Proyecto Calculadora de Notas en Java
-// Integrador: inicializar el proyecto en GitHub ademas de proteger la rama Main
-// Dev 1 Calcular: promedio de 3 notas // Dev 2 Indicar si aprueba o reprueba
-// Dev 3 Calcular nota necesaria en el final
-// Dev 4 Clasificar desempeño: bajo, medio, alto
-// Dev 5: Determinar la nota más alta y la nota más baja entre las tres calificaciones.
-// Dev 6: Identificar cuál de las tres notas requiere mayor mejora para aumentar el promedio general del estudiante.
 
 public class Main {
-    Scanner input = new Scanner(System.in);
-    void main(){
-        IO.println("CALCULADORA DE NOTAS");
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("CALCULADORA DE NOTAS");
 
         System.out.print("Ingrese el nombre del estudiante: ");
         String nombre = input.nextLine();
+
         System.out.print("Ingrese la nota 1: ");
         double nota1 = input.nextDouble();
+
         System.out.print("Ingrese la nota 2: ");
         double nota2 = input.nextDouble();
+
         System.out.print("Ingrese la nota 3: ");
         double nota3 = input.nextDouble();
-        IO.println();
 
-        // Funcionalidades del proyecto
+        System.out.println();
+
         dev1(nota1, nota2, nota3);
         dev2(nota1, nota2, nota3);
         dev3(nota1, nota2, nota3);
@@ -34,29 +32,29 @@ public class Main {
         input.close();
     }
 
-void dev1(double nota1, double nota2, double nota3) {
+    static void dev1(double nota1, double nota2, double nota3) {
 
-}
+    }
 
-void dev2(double nota1, double nota2, double nota3) {
+    static void dev2(double nota1, double nota2, double nota3) {
 
-}
+    }
 
-void dev3(double nota1, double nota2, double nota3) {
+    static void dev3(double nota1, double nota2, double nota3) {
 
-}
+    }
 
-void dev4(double nota1, double nota2, double nota3) {
+    static void dev4(double nota1, double nota2, double nota3) {
 
-}
+    }
 
-void dev5(double nota1, double nota2, double nota3) {
+    static void dev5(double nota1, double nota2, double nota3) {
 
-}
+    }
 
-void dev6(String nombre, double nota1, double nota2, double nota3) {
+    static void dev6(String nombre, double nota1, double nota2, double nota3) {
 
-        System.out.println("");
+        System.out.println();
 
         System.out.println("Hola " + nombre + ".");
         System.out.println("Tus notas son:");
@@ -77,5 +75,4 @@ void dev6(String nombre, double nota1, double nota2, double nota3) {
 
         System.out.println("Si mejoras esta nota, podrás aumentar tu promedio general.");
     }
-
 }
