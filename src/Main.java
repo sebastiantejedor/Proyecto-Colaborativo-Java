4ef8f06ff7c1df20cc6f4d9197a2e7fbd5183c23
Sebastian Tejedor Montiel <stejedorm@unicartagena.edu.co>
2026-06-07 15:04:06 -0500

    Actualizando Calculadora con entradas de usuario

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,81 @@
+import java.util.Scanner;
 // Git y Github Colaborativo Actividad
 // Proyecto Calculadora de Notas en Java
-
 // Integrador: inicializar el proyecto en GitHub ademas de proteger la rama Main
-// Dev 1 Calcular: promedio de 3 notas
-// Dev 2 Indicar si aprueba o reprueba
+// Dev 1 Calcular: promedio de 3 notas // Dev 2 Indicar si aprueba o reprueba
 // Dev 3 Calcular nota necesaria en el final
 // Dev 4 Clasificar desempeño: bajo, medio, alto
 // Dev 5: Determinar la nota más alta y la nota más baja entre las tres calificaciones.
 // Dev 6: Identificar cuál de las tres notas requiere mayor mejora para aumentar el promedio general del estudiante.
 
-void main() {
-    IO.println("===Java Calculator of Grades===");
-    dev1();
-    dev2();
-    dev3();
-    dev4();
-    dev5();
-    dev6(4.0, 2.5, 3.9);
-}
+public class Main {
+    Scanner input = new Scanner(System.in);
+    void main(){
+        IO.println("CALCULADORA DE NOTAS");
 
-void dev1() {
-    IO.println("promedio de 3 notas");
-}
+        System.out.print("Ingrese el nombre del estudiante: ");
+        String nombre = input.nextLine();
+        System.out.print("Ingrese la nota 1: ");
+        double nota1 = input.nextDouble();
+        System.out.print("Ingrese la nota 2: ");
+        double nota2 = input.nextDouble();
+        System.out.print("Ingrese la nota 3: ");
+        double nota3 = input.nextDouble();
+        IO.println();
 
-void dev2() {
-    IO.println("Indicar si aprueba o reprueba");
-}
+        // Funcionalidades del proyecto
+        dev1(nota1, nota2, nota3);
+        dev2(nota1, nota2, nota3);
+        dev3(nota1, nota2, nota3);
+        dev4(nota1, nota2, nota3);
+        dev5(nota1, nota2, nota3);
+        dev6(nombre, nota1, nota2, nota3);
 
-void dev3() {
-    IO.println("Calcular nota necesaria en el final");
-}
-
-void dev4() {
-    IO.println("Clasificar desempeño: bajo, medio, alto");
-}
-
-void dev5() {
-    IO.println("Determinar la nota más alta y la nota más baja entre las tres calificaciones.");
-}
-
-void dev6(double nota1, double nota2, double nota3) {
-    String name = "Pepito";
-
-    IO.println("Hello " + name + "! Vamos a calcular q nota debes reforzar para subir tu promedio general.");
-    IO.println("Tus notas son: " + nota1 + ", " + nota2 + " y " + nota3);
-    double menor = Math.min(nota1, Math.min(nota2, nota3));
-
-    if (menor == nota1) {
-        IO.println("La nota que debes reforzar es la nota 1: " + nota1);
-    } else if (menor == nota2) {
-        IO.println("La nota que debes reforzar es la nota 2: " + nota2);
-    } else {
-        IO.println("La nota que debes reforzar es la nota 3: " + nota3);
+        input.close();
     }
-    IO.println("¡Ánimo " + name + "! Con esfuerzo y dedicación puedes mejorar tu promedio general.");
+
+void dev1(double nota1, double nota2, double nota3) {
+
+}
+
+void dev2(double nota1, double nota2, double nota3) {
+
+}
+
+void dev3(double nota1, double nota2, double nota3) {
+
+}
+
+void dev4(double nota1, double nota2, double nota3) {
+
+}
+
+void dev5(double nota1, double nota2, double nota3) {
+
+}
+
+void dev6(String nombre, double nota1, double nota2, double nota3) {
+
+        System.out.println("");
+
+        System.out.println("Hola " + nombre + ".");
+        System.out.println("Tus notas son:");
+
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Nota 3: " + nota3);
+
+        double menor = Math.min(nota1, Math.min(nota2, nota3));
+
+        if (menor == nota1) {
+            System.out.println("La nota que requiere mayor mejora es la Nota 1: " + nota1);
+        } else if (menor == nota2) {
+            System.out.println("La nota que requiere mayor mejora es la Nota 2: " + nota2);
+        } else {
+            System.out.println("La nota que requiere mayor mejora es la Nota 3: " + nota3);
+        }
+
+        System.out.println("Si mejoras esta nota, podrás aumentar tu promedio general.");
+    }
+
 }
